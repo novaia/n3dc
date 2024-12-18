8fa39c36d115aa8e982807ad5ead929dab6f860d
hayden-donnelly <hayden.git@proton.me>
2024-12-18 13:47:54 -0500

    Transferred copyright to Hayden Donnelly.

--- libs/n3dc_obj.h
+++ libs/n3dc_obj.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright 2024 Novaia, Inc.
+ * Copyright 2024 Hayden Donnelly
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
